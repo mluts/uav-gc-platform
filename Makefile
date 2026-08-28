7f44afada4436a23d6b93f9c4408789e2e2e637d
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-08-28 14:51:19 +0300

    typings again

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: run uav-sitl rm-uav-sitl check stubs
+.PHONY: run uav-sitl rm-uav-sitl check repl
 
 run: uav-sitl
 	docker run -it --rm -p 127.0.0.1:5762:5762 uav-sitl
@@ -6,11 +6,14 @@ run: uav-sitl
 check:
 	./.venv/bin/python3 ./scripts/check_sitl.py
 
-stubs:
-	pyright --createstub pymavlink
+repl:
+	PYTHONSTARTUP="$(CURDIR)/.pythonstartup.py" ./.venv/bin/python3
 
 uav-sitl:
 	docker build -t uav-sitl sitl
 
 rm-uav-sitl:
 	docker image rm uav-sitl
+
+ardupilotmega:
+	nvim "$$(./.venv/bin/python3 -c 'import pymavlink; from pathlib import Path; print(Path(pymavlink.__file__).parent / "dialects/v20/ardupilotmega.py")')"
--- scripts/check_sitl.py
+++ scripts/check_sitl.py
@@ -5,30 +5,58 @@ import sys
 from pymavlink import mavutil
 
 
-def step(name, ok, detail=""):
-    status = "PASS" if ok else "FAIL"
+class Check:
+    indent = ""
 
-    print(f"[{status}] {name}" + (f" - {detail if detail else ''}"))
+    def __indent(self):
+        print(self.indent, end="")
 
-    if not ok:
-        sys.exit(1)
+    def log(self, *msg):
+        self.__indent()
+        print(*msg)
+
+    def step(self, name, ok, detail=""):
+        status = "PASS" if ok else "FAIL"
+
+        self.__indent()
+        print(f"[{status}] {name}" + (f" - {detail if detail else ''}"))
+
+        if not ok:
+            sys.exit(1)
+
+        self.indent += " " * 2
 
 
-conn = mavutil.mavlink_connection("tcp:127.0.0.1:5762")
+check = Check()
+
+check.log("checking heartbeat...")
+
+conn = mavutil.mavlink_connection(
+    "tcp:127.0.0.1:5762",
+    dialect="ardupilotmega",
+)
+
 # https://mavlink.io/en/messages/common.html#HEARTBEAT
 hb = conn.wait_heartbeat(timeout=10)
 
-step("hearbeat", hb is not None, f"sysid={conn.target_system}")
+check.log(f"mavlink_version={conn.WIRE_PROTOCOL_VERSION}")
 
-ready = False
-while not ready:
-    # https://mavlink.io/en/messages/common.html#SYS_STATUS
-    s = conn.recv_match(type="SYS_STATUS", blocking=True, timeout=5)
-    # https://mavlink.io/en/messages/common.html#MAV_SYS_STATUS_SENSOR
-    # health ∋ (enabled ∋ present)
-    # 1 ok, 0 not ok
-    ready = s and (
-        s.onboard_control_sensors_health & mavutil.mavlink.MAV_SYS_STATUS_PREARM_CHECK
-    )
+check.step("hearbeat", hb is not None, f"sysid={conn.target_system}")
 
-step("prearm check", ready, f"sysid={conn.target_system}")
+
+check.log("checking prearm readiness...")
+
+conn.mav.command_long_send(
+    # conn.target_system, conn.target_component, mavutil.mavlink.command_long_send
+)
+
+# https://mavlink.io/en/messages/common.html#SYS_STATUS
+s = conn.recv_match(type="SYS_STATUS", blocking=True, timeout=10)
+# https://mavlink.io/en/messages/common.html#MAV_SYS_STATUS_SENSOR
+# health ∋ (enabled ∋ present)
+# 1 ok, 0 not ok
+ready = s and (
+    s.onboard_control_sensors_health & mavutil.mavlink.MAV_SYS_STATUS_PREARM_CHECK
+)
+
+check.step("prearm check", ready, f"sysid={conn.target_system}")
